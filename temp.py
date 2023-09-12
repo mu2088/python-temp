ac236a14ec3b9f1a3afed6be0597996c9759cd99
mu2088 <mustafabaker4040gmail.com>
2023-09-12 11:29:14 +0200

    Python module for Rasberry

--- temp.py
+++ temp.py
@@ -1,7 +1,15 @@
 #! /usr/bin/python
 
 TEMP_PATH="/sys/class/thermal/thermal_zone0/"
-fo = open(TEMP_PATH + "temp","r")
-temp = fo.read()
-print(temp)
 
+def readTemp():
+    fo = open(TEMP_PATH + "temp","r")
+    temp = fo.read()
+    return int(temp)
+
+def averageTemp(n):
+    sum=0
+    for i in range(n):
+        sum=sum+readTemp()
+    average = sum/n
+    return average
